60076310ed04b175d05a1b17e1a511a7099b20b2
slorello89 <slorello89@gmail.com>
2019-12-10 16:52:23 -0500

    changing listen to work with heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
  
-app.listen(3000)
+app.listen(process.env.PORT || 5000)
 
 app.post('/webhooks/inbound-message', (req, res) => {
     console.log(req.body);
