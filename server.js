364cf8a3433b44125ff41e15d7b1cc1e366cc756
slorello89 <slorello89@gmail.com>
2019-12-11 08:22:06 -0500

    adding response logic

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
+
+const nexmo = new Nexmo({
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET
+});
  
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
  
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
 
 app.post('/webhooks/inbound-message', (req, res) => {
-    console.log(req.body['text']);
+    console.log(req.body);
+
+    var text = req.body['text'];
+
+    name = text.split('Hello my name is ')[1]
+
+    to = req.body['msisdn']
+    from = req.body['to']
+
+    responseText = "Hello " + name + " this is Nexmo!"
+
+    nexmo.message.sendSms(from, to, responseText, {
+      type: "unicode"
+    }, (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (responseData.messages[0]['status'] === "0") {
+          console.log("Message sent successfully.");
+        } else {
+          console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+        }
+      }
+    })
+
    
     res.status(200).end();
   });
